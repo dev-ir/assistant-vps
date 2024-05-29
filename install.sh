175aeb05776bd832827d4b94d726fb4196012fd3
HO3EIN <info@developermen.ir>
2024-05-29 20:18:20 +0330

    Update install.sh
    CHANGE COLOR AND READ OPTION FROM USER

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+#add color for text
 red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
 
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
@@ -8,6 +15,9 @@ red='\033[0;31m'
 wellcome(){
     echo -e "Hi , welcome to dev-ir script"
     echo -e "1- install speedtest on IR VPS"
+    echo -e "${green}Please choose an option:${red}"
+
+    read -p "Enter option number: " choice
 }
 
 wellcome
