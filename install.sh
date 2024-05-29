6c828c07994db60401d4fbd79264b6a6f6e21788
unknown <info@developermen.ir>
2024-05-29 20:12:25 +0330

    add color red and more

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+#add color for text
 red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+cur_dir=$(pwd)
+
 
 # check root
 [[ $EUID -ne 0 ]] && echo -e "${red}Fatal error: ${plain} Please run this script with root privilege \n " && exit 1
